e689234d88ed0eaae4caa6e7fec382635e36beee
Arief Gusti <arief@hellonimbly.com>
2020-10-04 18:35:19 +0700

    gallery styling

--- app/components/Gallery.js
+++ app/components/Gallery.js
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 
 class Gallery extends Component {
     render(){
+        let noImage = 'https://cdn4.iconfinder.com/data/icons/solid-part-6/128/image_icon-512.png';
+
         return(
             <div>
                 {
                     this.props.items.map((item, index) => {
-                        let {title} = item.volumeInfo;
+                        let {title, imageLinks, infoLink} = item.volumeInfo;
                         return(
-                            <div key={index}>{title}</div>
+                            <a 
+                                key={index} 
+                                className="book"
+                                href={infoLink}
+                                target="_blank"
+                            >
+                                <img 
+                                    src={imageLinks !== undefined ? imageLinks.thumbnail : noImage} 
+                                    alt={title}
+                                    className="book-img"
+                                />
+                                <div className="book-title">
+                                    {title}
+                                </div>
+                                
+                                
+                            </a>
                         )
                     })
                 }
